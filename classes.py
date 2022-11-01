bd376a9ca55bd02d22270c0492f1c56efad78dd7
ismail <aithsaine3@gmail.com>
2022-11-01 17:37:21 +0100

    v1

---
+++ classes.py
@@ -0,0 +1,72 @@
+from pymongo import MongoClient
+
+class Stage:
+    cnx = MongoClient("localhost",27017)
+    db = cnx["db_stages"]
+    stages_collection = db["stage"]
+
+
+    def __init__(self,id:int, nom:str, duree:int,prix:float,domaine:str) -> None:
+        self.__id = id
+        self.__nom = nom
+        self.__duree = duree
+        self.__prix = prix
+        self.__domaine = domaine
+
+    def getId(self):
+        return self.__id
+    property(getId)
+
+    def getNom(self):
+        return self.__nom
+
+    def setNom (self,new_name):
+        self.__nom = new_name
+
+    property(getNom,setNom)
+
+    def getDuree(self):
+        return self.__duree
+    def setDuree (self,new_duree):
+        self.__duree=new_duree    
+    property(getDuree, setDuree)    
+
+
+    def getPrix(self):
+        return self.__prix
+    def setPrix (self,new_prix):
+        self.__prix=new_prix    
+    property(getPrix, setPrix)    
+
+    def getDomaine(self):
+        return self.__domaine
+    def setDomaine (self,new_d__domaine):
+        self.__domaine=new_d__domaine    
+    property(getDomaine, setDomaine)
+
+# 
+    def insertToDb(self):
+        Stage.stages_collection.insert_one({"id":self.__id,"nom":self.__nom,"duree":self.__duree,"prix":self.__prix,"domaine":self.__domaine})
+
+    @staticmethod
+    def getAllStages()->list:
+        res = list(Stage.stages_collection.find({}))
+        return res
+    
+    @staticmethod
+    def UpdateFromDb(id:int,new_data:dict=None):
+        Stage.stages_collection.update_one({"id":id},{"$set":new_data})
+
+    @staticmethod
+    def DeleteFromDb(id:int):
+        Stage.stages_collection.delete_one({"id":id})
+
+
+
+""" stages = Stage.getAllStages()
+for item in stages:
+    for key,value in dict(item).items():
+        print(key,value,sep="  :  ")
+    print() """
+
+""" res = Stage.UpdateFromDb(2,{"nom":"Generate DataBases SQL"}) """
---
+++ main.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+from classes import Stage
+from tkinter import messagebox
+from tkinter import ttk
+import re
+
+
+
+
+app = tk.Tk()
+
+
+app.geometry("500x500")
+app.iconbitmap("stage.ico")
+app.title("Gestion Des Stage")
+app.resizable(False,False)
+lb_title = tk.Label(text="Gestion De Stage",font="5",bd=10)
+lb_title.grid(column=0,row=0,columnspan=5,rowspan=3,pady=10,padx=120)
+# id :
+lb_id = tk.Label(text="Id",font=10)
+lb_id.grid(column=0,row=4,padx=10,sticky=tk.W)
+enId = tk.Entry(font=15,bd=2)
+enId.grid(row=4,column=1,padx=20)
+#  nom stage : 
+lb_name = tk.Label(text="Nom de stage",font=10)
+lb_name.grid(column=0,row=5,padx=10,sticky=tk.W)
+enname = tk.Entry(font=15,bd=2)
+enname.grid(row=5,column=1)
+#  duree : 
+lb_Duration = tk.Label(text="Duree",font=10)
+lb_Duration.grid(column=0,row=6,padx=10,sticky=tk.W)
+enDuration = tk.Entry(font=10,bd=2)
+enDuration.grid(row=6,column=1)
+
+# prix
+lb_price = tk.Label(text="Prix",font=10)
+lb_price.grid(column=0,row=7,padx=10,sticky=tk.W)
+enPrice = tk.Entry(font=10,bd=2)
+enPrice.grid(row=7,column=1)
+# domaine
+lb_Domaine = tk.Label(text="Domaine",font=10)
+lb_Domaine.grid(column=0,row=8,padx=10,sticky=tk.W)
+enDomaine = tk.Entry(font=10,bd=2)
+enDomaine.grid(row=8,column=1)
+
+# btns Function:
+
+
+def Inserer():
+    id = enId.get()
+    nom = enname.get()
+    duree = enDuration.get()
+    prix = enPrice.get()
+    domaine = enDomaine.get()
+    if id=="" or nom =="" or duree=="" or prix == "" or domaine == "":
+        messagebox.showerror("Error","entrer tout les information de stage")
+
+    else:
+        ok = True
+        if not re.search(r"^[0-9]$",id) or not re.search(r"[-+]?\d*\.?\d+",prix) or not re.search(r"^[0-9]",duree):
+            messagebox.showerror("Error","l'id et duree et le prix doit etre des numero")
+            ok = False
+        if ok:
+            stg = Stage(int(id),nom,int(duree),float(prix),domaine)
+            stg.insertToDb()
+            messagebox.showinfo("congratulation", "data saved with success")
+
+
+
+#btn insert
+
+
+btnInsert = tk.Button(text="Inserer",bg="#c1eeff",width=10,command=Inserer).grid(row=4, column=2,pady=5)
+btnDelete = tk.Button(text="Supprimer",bg="#c1eeff",width=10).grid(row=5, column=2,pady=5)
+btnSearsh = tk.Button(text="Chercher",bg="#c1eeff",width=10).grid(row=6, column=2,pady=5)
+btnUpdate = tk.Button(text="Modifier",bg="#c1eeff",width=10).grid(row=7, column=2,pady=5)
+dd = tk.Checkbutton()
+table = ttk.Treeview(app,columns=(1,2,3,4,5),heigh=4,show="headings",padding=5)
+table.heading(1,text="ID")
+table.heading(2,text="nom")
+table.heading(3,text="duration")
+table.heading(4,text="prix")
+table.heading(5,text="domaine")
+table.column(1,width=50)
+table.column(2,width=200)
+table.column(3,width=60)
+table.column(4,width=70)
+table.column(5,width=100)
+table.place(x=3,y=300)
+
+stages = Stage.getAllStages()
+liste_des_tupple_stage = []
+for item in stages:
+    liste_des_tupple_stage.append((item['id'],item['nom'],item['duree'],item['prix'],item['domaine']))
+for item in liste_des_tupple_stage:
+    table.insert("",tk.END,values=item)
+app.mainloop()
